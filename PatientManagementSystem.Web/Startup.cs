0f75320df923171922193a5ce73e409b01b83d10
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-07-27 10:37:07 +0300

    Created initial project

---
+++ PatientManagementSystem.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PatientManagementSystem.Web.Startup))]
+namespace PatientManagementSystem.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
