1a563e17a8a5bc5686e0c8d80b756813cec52521
Tudor Anghelina <tudor.anghelina91@gmail.com>
2019-08-19 20:41:46 +0300

    Removed Delete feature on MedicalRecordEntry since it randomly gives a Null type of error and the model state is never valid

--- PatientManagementSystem/PatientManagementSystem.Web/Areas/DoctorArea/Controllers/MedicalRecordsController.cs
+++ PatientManagementSystem/PatientManagementSystem.Web/Areas/DoctorArea/Controllers/MedicalRecordsController.cs
@@ -96,30 +96,32 @@ namespace PatientManagementSystem.Web.Areas.DoctorArea.Controllers
             return View(medicalRecordEntryViewModel);
         }
 
-        public ActionResult Delete(int id, int patientId)
-        {
-            MedicalRecordEntryViewModel medicalRecordEntryViewModel = medicalRecordEntryRepository.GetById(id).ToViewModel();
-            medicalRecordEntryViewModel.PatientViewModel = patientRepository.GetById(patientId).ToViewModel();
-            AddPatientToTempData(patientId);
-            return View(medicalRecordEntryViewModel);
-        }
+        //public ActionResult Delete(int id, int patientId)
+        //{
+        //    MedicalRecordEntryViewModel medicalRecordEntryViewModel = medicalRecordEntryRepository.GetById(id).ToViewModel();
+        //    medicalRecordEntryViewModel.PatientViewModel = patientRepository.GetById(patientId).ToViewModel();
+        //    medicalRecordEntryViewModel.PatientViewModel.MedicalRecord = medicalRecordEntryRepository.GetForPatient(patientId).ToViewModel();
+        //    AddPatientToTempData(patientId);
+        //    return View(medicalRecordEntryViewModel);
+        //}
 
-        [HttpPost]
-        public ActionResult Delete(MedicalRecordEntryViewModel medicalRecordEntryViewModel, int patientId)
-        {
-            medicalRecordEntryViewModel.PatientViewModel = patientRepository.GetById(patientId).ToViewModel();
-            //AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
+        //[HttpPost]
+        //public ActionResult Delete(MedicalRecordEntryViewModel medicalRecordEntryViewModel, int patientId)
+        //{
+        //    medicalRecordEntryViewModel.PatientViewModel = patientRepository.GetById(patientId).ToViewModel();
+        //    medicalRecordEntryViewModel.PatientViewModel.MedicalRecord = medicalRecordEntryRepository.GetForPatient(patientId).ToViewModel();
+        //    AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
 
-            medicalRecordEntryRepository.Delete(medicalRecordEntryViewModel.ToDomainModel());
-            return RedirectToAction("Index", new { patientId = patientId });
-            //if (ModelState.IsValid)
-            //{
-            //    AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
-            //    medicalRecordEntryRepository.Delete(medicalRecordEntryViewModel.ToDomainModel());
-            //    return RedirectToAction("Index", new { patientId = medicalRecordEntryViewModel.PatientViewModel.Id });
-            //}
-            //AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
-            //return View(medicalRecordEntryViewModel);
-        }
+        //    //medicalRecordEntryRepository.Delete(medicalRecordEntryViewModel.ToDomainModel());
+        //    //return RedirectToAction("Index", new { patientId = patientId });
+        //    if (ModelState.IsValid)
+        //    {
+        //        AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
+        //        medicalRecordEntryRepository.Delete(medicalRecordEntryViewModel.ToDomainModel());
+        //        return RedirectToAction("Index", new { patientId = medicalRecordEntryViewModel.PatientViewModel.Id });
+        //    }
+        //    AddPatientToTempData(medicalRecordEntryViewModel.PatientViewModel.Id);
+        //    return View(medicalRecordEntryViewModel);
+        //}
     }
 }
